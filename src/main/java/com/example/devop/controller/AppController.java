890f42b88965bbd0abe8ca1d98fdd334650dffbb
Maadhini <maadhinimanivannan@gmail.com>
2025-04-02 11:50:07 +0530

    initial commit

---
+++ src/main/java/com/example/devop/controller/AppController.java
@@ -0,0 +1,29 @@
+package com.example.devop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+        return "products";
+    }
+}
---
+++ src/test/java/com/example/devop/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
